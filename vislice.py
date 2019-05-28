659f72ac6f9d4b27efaab97ff2ec82cc0f8c76d6
Tom Rupnik Medjedovič <tom.rupnik-medjedovic@student.fmf.uni-lj.si>
2019-05-28 19:14:44 +0200

    koncno

--- vislice.py
+++ vislice.py
@@ -1,7 +1,7 @@
 import bottle
 import model
 
-vislice= model.Vislice()
+vislice= model.Vislice('stanje.json')
 
 @bottle.get("/") 
 def index():
@@ -22,6 +22,34 @@ def ugibaj(id_igre):
     crka= bottle.request.forms.getunicode('crka')
     vislice.ugibaj(id_igre, crka)
     bottle.redirect('/igra/{}/'.format(id_igre))    
+
+
+
+@bottle.post('/nova_igra/')
+def nova_igra():
+    id_igre = vislice.nova_igra()
+    bottle.response.set_cookie('id_igre', str(id_igre), path='/')
+    bottle.redirect('/igra/')
+
+
+@bottle.get('/igra/')
+def pokazi_igro():
+    id_igre = int(bottle.request.get_cookie('id_igre'))
+    igra, stanje = vislice.igre[id_igre]
+    return bottle.template('igra.tpl',igra=igra, stanje=stanje ,id_igre=id_igre)
+
+
+@bottle.post('/igra/')
+def ugibaj_2():
+    id_igre= int(bottle.request.get_cookie('id_igre'))
+    crka= bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra/')
+
+
+
+
+
 
 @bottle.get('/img/<picture>')
 def serve_picture(picture):
